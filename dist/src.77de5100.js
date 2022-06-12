0b6785ccc470e0f6aa2a932c8de2a9d360f547b4
stein820 <90308864+stein820@users.noreply.github.com>
2022-06-12 15:11:47 +0700

    update UI

--- dist/src.77de5100.js
+++ dist/src.77de5100.js
@@ -2358,7 +2358,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "23980" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54577" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
